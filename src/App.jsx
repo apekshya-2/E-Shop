ff12a1539561e592ab3befe807095a480a4d5412
Apeksha Dhungana <dipikadhungana86@gmail.com>
2025-08-27 18:27:22 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import axios from 'axios'
 
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 
